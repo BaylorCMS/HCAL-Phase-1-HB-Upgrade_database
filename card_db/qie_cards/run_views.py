046a400f5a9548c38d30cdda594920b9cf61b82f
Caleb Smith <colorado.caleb.smith@gmail.com>
2018-06-28 11:31:19 -0500

    Put plots for each card for a certain test in the runs page

--- card_db/qie_cards/run_views.py
+++ card_db/qie_cards/run_views.py
@@ -50,14 +50,26 @@ def card_plots(request, run, card):
     """This displays the test plots relating to a card"""
     test_types = list(Attempt.objects.filter(run=run).order_by('test_type__name'))
     tests = []
-    attempts = []
     for attempt in test_types:
         if attempt.test_type not in tests:
             tests.append(attempt.test_type)
     
+    attempts = []
     for test in tests:
         attempts.append(Attempt.objects.filter(run=run, card__barcode=card, test_type=test).last())
             
-                        
-    
     return render(request, 'runs/card_plots.html', {'test_list': tests, 'attempt_list': attempts}) 
+
+def test_plots(request, run, test):
+    """This displays the plots relating tied to a specific test"""
+    card_names = list(Attempt.objects.filter(run=run).order_by('card__barcode'))
+    cards = []
+    for attempt in card_names:
+        if attempt.card not in cards:
+            cards.append(attempt.card)
+
+    attempts = []
+    for card in cards:
+        attempts.append(Attempt.objects.filter(run=run, test_type__name=test, card__barcode=card).last())
+
+    return render(request, 'runs/test_plots.html', {'card_list': cards, 'attempt_list': attempts})
