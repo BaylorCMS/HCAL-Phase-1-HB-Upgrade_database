a7586ba6643301cf4de626f68144aedc8d39fad5
Caleb Smith <colorado.caleb.smith@gmail.com>
2018-07-09 15:25:47 -0500

    added the search urls

--- card_db/qie_cards/card_urls.py
+++ card_db/qie_cards/card_urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     url(r'^(?P<card>[0-9]{7})/calibration/(?P<group>[0-9]{1,2})/plots$', views.calPlots, name='plotview'),
     url(r'^(?P<card>[0-9]{7})/calibration/(?P<group>[0-9]{1,2})/results$', views.calResults, name='results'),
     url(r'^(?P<card>[0-9]{7})/(?P<test>.*)$', views.testDetail, name='testDetail'),
+    url(r'^search$', views.search, name='search'),
+    url(r'^search/(?P<query>.*)$', views.searchbar, name='searchbar'),
     url(r'^error$', views.error, name='error'),
     url(r'^media/(?P<path>.*)$', serve, {'document_root':MEDIA_ROOT,'show_indexes':True}),
     url(r'^plots$', views.PlotView.as_view(), name='plots'),
