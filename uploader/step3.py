1f2683cfd30425074a607b24441e95c247c754ec
Caleb Smith <colorado.caleb.smith@gmail.com>
2018-07-31 16:14:40 -0500

    add extension=1 and fix extension spelling

--- uploader/step3.py
+++ uploader/step3.py
@@ -53,8 +53,9 @@ def moveFile(qie, fileName):
     path = os.path.join(MEDIA_ROOT, url)
     if not os.path.exists(path):
         exit("Database does not contain this card's log folder")
+    extension = 1
     while os.path.isfile(os.path.join(path,  str(extension) + os.path.basename(fileName))):
-            extension += 1
+        extension += 1
         newPath = os.path.join(path,  str(extension) + os.path.basename(fileName))
     newPath = os.path.join(path, os.path.basename(fileName))
     copyfile(fileName, newPath)
